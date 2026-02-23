f1b3a6f4e31e5335e371daef927d5b9eb7581a17
aliekberayhan <aliekberayhan09@hotmail.com>
2026-02-23 05:17:15 +0300

    delete

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -6,12 +6,12 @@ import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
 
 export const appRoutes: Routes = [
+    { path: '', component: Landing },
     {
         path: '',
         component: AppLayout,
         children: [
             { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
